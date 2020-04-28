96cc851784a3df37df8847ee2d3ed1ff202c49fa
Ubuntu <ubuntu@ip-172-31-14-98.us-west-2.compute.internal>
2020-04-28 09:52:12 +0000

    updates

--- download_and_extract_images.sh
+++ download_and_extract_images.sh
@@ -2,9 +2,14 @@
 
 cd ~/
 mkdir -p data/super-resolution/orig
+mkdir data/super-resolution/converted_images
+mkdir data/super-resolution/converted_images/small
+mkdir data/super-resolution/converted_images/med
+mkdir data/super-resolution/converted_images/large
 wget -P data/super-resolution/orig http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
 cd data/super-resolution/orig/
 tar -xvf VOCtrainval_06-Nov-2007.tar
 mv VOCdevkit/VOC2007/JPEGImages .
 rm -rf VOCdevkit/
+
 
